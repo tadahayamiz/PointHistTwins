e9b6e824a0facd5c5eac8ee6d82263d7e60ebafd
tadahayamiz <tadahaya@gmail.com>
2025-03-23 10:12:34 +0900

    debug

--- phtwins/core.py
+++ phtwins/core.py
@@ -148,7 +148,7 @@ class PHTwins:
         with torch.no_grad():
             for i in indices:
                 data, _ = dataset[i]
-                hist0, hist1 = (x.to(self.device) for x in data)
+                hist0, hist1 = (x.to(self.config["device"]) for x in data)
                 (z1, z2), _ = self.pretrained_model(hist0, hist1)
                 output = (z1 + z2) / 2 # average two features
                 reps.append(output.cpu().numpy())
